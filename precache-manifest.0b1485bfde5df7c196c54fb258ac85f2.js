6bf8e2477ae1eed96a3164ca789fc772bfb6b6f5
aditya3331 <tripathiaditya196@gmail.com>
2020-07-07 20:57:15 +0530

    Updates

---
+++ precache-manifest.0b1485bfde5df7c196c54fb258ac85f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1be3b574130f9d256f36bf3753ec55cc",
+    "url": "/Monsters-Roladex/index.html"
+  },
+  {
+    "revision": "3c8f08919f07f1ac379c",
+    "url": "/Monsters-Roladex/static/css/main.d31ae58e.chunk.css"
+  },
+  {
+    "revision": "ddfc98fd1fb7cc1baeab",
+    "url": "/Monsters-Roladex/static/js/2.fcb60162.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters-Roladex/static/js/2.fcb60162.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c8f08919f07f1ac379c",
+    "url": "/Monsters-Roladex/static/js/main.d73a3430.chunk.js"
+  },
+  {
+    "revision": "fa8065ae714da654b320",
+    "url": "/Monsters-Roladex/static/js/runtime-main.ac4838ae.js"
+  }
+]);
